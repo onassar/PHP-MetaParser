c7744e2f6e99171e61a8d88ce0076ef24ffcadb1
Oliver Nassar <onassar@gmail.com>
2011-07-31 12:13:46 -0400

    Intial commit

---
+++ MetaParser.class.php
@@ -0,0 +1,570 @@
+<?php
+/*
+    Sample cases:
+        http://metallo.scripps.edu/
+        http://jobs.businessinsider.com/job/3b9d1c8e1e5e31d4e4fefb4a551b8b90/?d=1&source=site_home
+        http://www.wikipedia.org
+        http://yahoo.com
+        http://twitter.com/wikileaks/status/8920530488926208#s
+        http://veryawesomeworld.com/awesomebook/inside.html
+*/
+
+    /**
+     * MetaParser class.
+     * 
+     * @todo add support paths leading with '//' (aka. use same scheme)
+     */
+    class MetaParser
+    {
+        /**
+         * _parsed
+         * 
+         * @var array
+         * @access protected
+         */
+        protected $_parsed;
+
+        /**
+         * _body.
+         * 
+         * @var string
+         * @access protected
+         */
+        protected $_body;
+
+        /**
+         * _url
+         * 
+         * @var string
+         * @access protected
+         */
+        protected $_url;
+
+        /**
+         * __construct function.
+         * 
+         * @access public
+         * @param string $body
+         * @param string $url
+         * @return void
+         */
+        public function __construct($body, $url)
+        {
+            $this->_body = $body;
+            $this->_url = $url;
+        }
+
+        /**
+         * _encode function.
+         * 
+         * @access protected
+         * @param mixed $mixed
+         * @return array
+         */
+        protected function _encode($mixed)
+        {
+            if (is_array($mixed)) {
+                foreach ($mixed as $key => $value) {
+                    $mixed[$key] = $this->_encode($value);
+                }
+                return $mixed;
+            }
+            return htmlentities($mixed, ENT_QUOTES, 'UTF-8');
+        }
+
+        /**
+         * resolveFullPath function.
+         * 
+         * @see http://ca3.php.net/manual/en/function.realpath.php#86384
+         * @access protected
+         * @param string $addr
+         * @param string $base
+         * @return string
+         */
+        protected function _resolveFullPath($addr, $base)
+        {
+            // empty address provided
+            if (empty($addr)) {
+                return $base;
+            }
+
+            // parse address; if scheme found, doesn't need to be resolved
+            $parsed = parse_url($addr);
+            if(array_key_exists('scheme', $parsed)) {
+                return $addr;
+            }
+
+            // parse base passed in (will always be a full url)
+            $parsed = parse_url($base);
+
+            // protocol specific
+            if (mb_substr($addr, 0, 2) === '//') {
+                return ($parsed['scheme']) . '://' . mb_substr($addr, 2);
+            }
+            // otherwise if the address should go to the top of the tree
+            elseif ($addr{0} === '/') {
+                return ($parsed['scheme']) . '://' . ($parsed['host']) .
+                    ($addr);
+            }
+
+            // if the address doesn't contain any sub-directory calls
+            if (!strstr($addr, '../')) {
+                return ($base) . ($addr);
+            }
+
+            // set-up sub-directory pieces for traversing/replacing
+            $pieces['addr'] = explode('../', $addr);
+            $pieces['base'] = explode('/', $parsed['path']);
+            array_pop($pieces['base']);
+            $count = count($pieces['addr']) - 1;
+
+            // array of respective sub-directory replacements (from base)
+            $replacements = array_slice($pieces['base'], 0, 0 - $count);
+            $replacements = array_filter($replacements);
+
+            // add last non-empty sub-directory as tail
+            $tail = array_pop($pieces['addr']);
+            if (!empty($tail)) {
+                $replacements[] = $tail;
+            }
+
+            // return sub-directory traversed address
+            return ($parsed['scheme']) . '://' . ($parsed['host']) .
+                '/' . implode('/', $replacements);
+        }
+
+        /**
+         * _parseBase function.
+         * 
+         * @todo find url's that have various base values, and test them
+         * @access private
+         * @return string
+         */
+        private function _parseBase()
+        {
+            // search for base tag; return empty string if none found
+            preg_match_all(
+                '/<base.*href=(\'|")(.*)\1.*>/imU',
+                $this->_body,
+                $bases
+            );
+
+            // store url components (will need to be used before value returned)
+            $components = parse_url($this->_url);
+            $path = ($components['scheme']) . '://' . ($components['host']);
+            if (isset($components['path'])) {
+                $path .= $components['path'];
+            } else {
+                $path .= '/';
+            }
+
+            // remove any filename that was specified by the path explicitely
+            $path = preg_replace('/[^\/]*$/U', '', $path);
+
+            // no base tags found
+            if (empty($bases[2])) {
+                return $path;
+            }
+
+            // set base
+            $base = trim(array_pop($bases[2]));
+
+            // set variable to check for target attribute value
+            $found = array_pop($bases[0]);
+
+            // if a target attribute found
+            if (preg_match('/target=/', $found)) {
+
+                // check if target being specified is the document itself (which
+                // is okay)
+                $self = preg_match('/target=[\'"]{1}_self[\'"]{1}/', $found);
+
+                // if it's not itself, set the base based on the url being
+                // 'grabbed'
+                if (!$self) {
+                    return $path;
+                }
+            }
+
+            // resolve path (check for trailing slash; always required)
+            $resolved = $this->_resolveFullPath($base, $path);
+            if (!preg_match('/\/$/', $resolved)) {
+                return ($resolved) . '/';
+            }
+            return $resolved;
+        }
+
+        /**
+         * _parseDescription function.
+         * 
+         * @note not checking the index of the regular expression that
+         *     corresponds to the actual keywords in order to ensure that an
+         *     actual meta tag for keywords was specified. This way I can return
+         *     false if the meta tag isn't there at all
+         * @note due to a bug, the second regex does *not* support newlines in
+         *     the meta tag content attribute values
+         * @access private
+         * @return string
+         */
+        private function _parseDescription()
+        {
+            // grab meta tag; return immediately if false
+            $description = $this->_parseMetaTag('description');
+            if ($description === false) {
+                return false;
+            }
+
+            // trim/return
+            return trim($description);
+        }
+
+        /**
+         * _parseFavicon function.
+         * 
+         * @access private
+         * @return string
+         */
+        private function _parseFavicon()
+        {
+            // generate default
+            $parsed = parse_url($this->_url);
+            $default = ($parsed['scheme']) . '://' . ($parsed['host']) . '/favicon.ico';
+
+            // get the page links (icon attribute value leading)
+            preg_match_all(
+                '/<link.+rel=(\'|").*[^-]\bicon\b.{0,20}href=(\'|")(.+)\2/imU',
+                $this->_body,
+                $favicons
+            );
+            if (empty($favicons[3])) {
+
+                // get the page links (icon attribute value trailing)
+                preg_match_all(
+                    '/<link.+href=(\'|")(.+)\1.{0,20}rel=(\'|").*[^-]\bicon\b/imU',
+                    $this->_body,
+                    $favicons
+                );
+
+                // no favicon found
+                if (empty($favicons[2])) {
+                    return $default;
+                }
+                $favicon = array_pop($favicons[2]);
+            } else {
+                $favicon = array_pop($favicons[3]);
+            }
+
+            // resolve full path
+            $favicon = trim($favicon);
+            $favicon = $this->_resolveFullPath($favicon, $this->getBase());
+            $favicon = str_replace(PHP_EOL, '', $favicon);
+            return $favicon;
+        }
+
+        /**
+         * _parseImages function.
+         * 
+         * @note first expression capture relates to [^*]* rather than .* as
+         *     this was excluding new lines
+         * @access private
+         * @return array
+         */
+        private function _parseImages()
+        {
+            // get the page images
+            preg_match_all('/<img[^*]*src=(\'|")(.+)\1/imU', $this->_body, $images);
+            if (empty($images[2])) {
+                return array();
+            }
+
+            // base the images
+            $images = array_pop($images);
+            $base = $this->getBase();
+            foreach ($images as &$image) {
+                $image = $this->_resolveFullPath($image, $base);
+                $image = str_replace(PHP_EOL, '', $image);
+            }
+            $images = array_unique($images);
+            $images = array_values($images);
+
+            // return images found
+            return $images;
+        }
+
+        /**
+         * _parseKeywords function.
+         * 
+         * @note not checking the index of the regular expression that
+         *     corresponds to the actual keywords in order to ensure that an
+         *     actual meta tag for keywords was specified. This way I can return
+         *     false if the meta tag isn't there at all
+         * @access private
+         * @return array
+         */
+        private function _parseKeywords()
+        {
+            // grab meta tag; return immediately if false
+            $keywords = $this->_parseMetaTag('keywords');
+            if ($keywords === false) {
+                return false;
+            }
+
+            // iterate over them, and set as array using comma as delimiter
+            $keywords = explode(',', $keywords);
+            foreach ($keywords as &$keyword) {
+                $keyword = trim($keyword);
+            }
+            return $keywords;
+        }
+
+        /**
+         * _parseMetaTag function.
+         * 
+         * @access private
+         * @param string $value
+         * @return false|string
+         */
+        private function _parseMetaTag($value, $attr = 'name')
+        {
+            // get the page meta-tag (name attribute leading)
+            preg_match_all(
+                '/<meta.+' . ($attr) . '=(\'|")(\bdc\.\b)?\b' . ($value) . '\b\1.+content=(\'|")(.*)\3/imU',
+                $this->_body,
+                $tags
+            );
+
+            // meta tag not found (not that it's empty, but not-found)
+            if (empty($tags[3])) {
+
+                // get the page meta-tag (name attribute trailing)
+                preg_match_all(
+                    '/<meta.+content=(\'|")(.*)\1.+' . ($attr) . '=(\'|")(\bdc\.\b)?\b' . ($value) . '\b\3.+/imU',
+                    $this->_body,
+                    $tags
+                );
+
+                // no meta-tag found
+                if (empty($tags[3])) {
+                    return false;
+                }
+
+                // return value found
+                return array_pop($tags[2]);
+            }
+            
+            // return meta-tag found
+            return array_pop($tags[4]);
+        }
+
+        /**
+         * _parseOpenGraphKeys function.
+         * 
+         * @access protected
+         * @return array
+         */
+        protected function _parseOpenGraphKeys()
+        {
+            preg_match_all('/([\'|"]{1})og:(.{1,10})\1/', $this->_body, $keys);
+            return array_pop($keys);
+        }
+
+        /**
+         * _parseTitle function.
+         * 
+         * @access private
+         * @return string
+         */
+        private function _parseTitle()
+        {
+            // get the page's title
+            preg_match('/<title[^>]*>([^<]+)<\/title>/i', $this->_body, $titles);
+            if (empty($titles)) {
+                return false;
+            }
+
+            // return title found
+            return trim(array_pop($titles));
+        }
+
+        /**
+         * getBase function.
+         * 
+         * @note do not need to check for false value after _parseFavicon as
+         *     default will always be returned (eg. domain.com/favicon.ico)
+         * @access public
+         * @return false|string
+         */
+        public function getBase()
+        {
+            // return base found, if any
+            if (isset($this->_parsed['base'])) {
+                return $this->_parsed['base'];
+            }
+
+            // parse base/return
+            $base = $this->_parseBase();
+            $this->_parsed['base'] = $base;
+            return $base;
+        }
+
+        /**
+         * getDescription function.
+         * 
+         * @access public
+         * @return false|string
+         */
+        public function getDescription()
+        {
+            // return description found, if any
+            if (isset($this->_parsed['description'])) {
+                return $this->_parsed['description'];
+            }
+
+            // parse description/return
+            $description = $this->_parseDescription();
+            if ($description === false) {
+                return false;
+            }
+            $description = $this->_encode($description, false);
+            $this->_parsed['description'] = $description;
+            return $description;
+        }
+
+        /**
+         * getDetails function.
+         * 
+         * @access public
+         * @return array
+         */
+        public function getDetails()
+        {
+            // return relevant meta data
+            return array(
+                'base' => $this->getBase(),
+                'favicon' => $this->getFavicon(),
+                'meta' => array(
+                    'description' => $this->getDescription(),
+                    'keywords' => $this->getKeywords()
+                ),
+                'images' => $this->getImages(),
+                'openGraph' => $this->getOpenGraph(),
+                'title' => $this->getTitle(),
+                'url' => $this->getUrl()
+            );
+        }
+
+        /**
+         * getFavicon function.
+         * 
+         * @note do not need to check for false value after _parseFavicon as
+         *     default will always be returned (eg. domain.com/favicon.ico)
+         * @access public
+         * @return false|string
+         */
+        public function getFavicon()
+        {
+            // return favicon found, if any
+            if (isset($this->_parsed['favicon'])) {
+                return $this->_parsed['favicon'];
+            }
+
+            // parse favicon/return
+            $favicon = $this->_parseFavicon();
+            $this->_parsed['favicon'] = $favicon;
+            return $favicon;
+        }
+
+        /**
+         * getImages function.
+         * 
+         * @access public
+         * @return false|array
+         */
+        public function getImages()
+        {
+            // return images found, if any
+            if (isset($this->_parsed['images'])) {
+                return $this->_parsed['images'];
+            }
+
+            // parse images/return
+            $images = $this->_parseImages();
+            $this->_parsed['images'] = $images;
+            return $images;
+        }
+
+        /**
+         * getKeywords function.
+         * 
+         * @access public
+         * @return false|array
+         */
+        public function getKeywords()
+        {
+            // return keywords found, if any
+            if (isset($this->_parsed['keywords'])) {
+                return $this->_parsed['keywords'];
+            }
+
+            // parse keywords if found; encode; return
+            $keywords = $this->_parseKeywords();
+            if ($keywords === false) {
+                return false;
+            }
+            $keywords = $this->_encode($keywords, false);
+            $this->_parsed['keywords'] = $keywords;
+            return $keywords;
+        }
+
+        /**
+         * getOpenGraph function.
+         * 
+         * @access public
+         * @return array
+         */
+        public function getOpenGraph()
+        {
+            $graph = array();
+            $keys = $this->_parseOpenGraphKeys();
+            foreach ($keys as $key) {
+                $graph[$key] = $this->_parseMetaTag('og:' . ($key), 'property');
+            }
+            return $graph;
+        }
+
+        /**
+         * getTitle function.
+         * 
+         * @access public
+         * @return false|string
+         */
+        public function getTitle()
+        {
+            // return title found, if any
+            if (isset($this->_parsed['title'])) {
+                return $this->_parsed['title'];
+            }
+
+            // parse title/return
+            $title = $this->_parseTitle();
+            if ($title === false) {
+                return false;
+            }
+            $title = $this->_encode($title, false);
+            $this->_parsed['title'] = $title;
+            return $title;
+        }
+
+        /**
+         * getUrl function.
+         * 
+         * @access public
+         * @return string
+         */
+        public function getUrl()
+        {
+            return $this->_url;
+        }
+    }
+
+?>
